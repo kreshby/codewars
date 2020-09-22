ce1025fcf005c60c2b4b5da0f798f07083093680
Vladimir Kukresh <kreshsoftware@gmail.com>
2020-09-22 18:46:44 +0300

    refactor: apply eslint

--- 1_reversed-strings.js
+++ 1_reversed-strings.js
@@ -1,14 +1,14 @@
 let newA;
 
 function reversedStrings(b) {
-  if (b.length == 0) {
+  if (b.length === 0) {
     newA = b;
   } else {
     for (let i = b.length - 1; i >= 0; i--) {
-      if (i == b.length - 1) {
+      if (i === b.length - 1) {
         newA = b[i];
       } else {
-        newA = newA + b[i];
+        newA += b[i];
       }
     }
   }
@@ -16,22 +16,23 @@ function reversedStrings(b) {
   return newA;
 }
 
-//console.log(reversedStrings(a));
+// console.log(reversedStrings(a));
 
-console.log(reversedStrings(""));
+console.log(reversedStrings(''));
 
 /**
  * другие способы решения задачи
- * 
+ *
  * https://vk.com/@devloger-tri-sposoba-perevernut-stroku-v-javascripte
  * String.prototype.split(), Array.prototype.reverse() и Array.prototype.join().
 
 Метод split() принимает на вход строку и преобразует в массив, разбивая ее по разделителю.
-Метод reverse() переворачивает элементы массива. Первый элемент становится последним, последний — первым.
+Метод reverse() переворачивает элементы массива. Первый элемент становится последним, 
+последний — первым.
 Метод join() соединяет все элементы массива в строку.
 
 function solution(str){
-  return str.split('').reverse().join('');  
+  return str.split('').reverse().join('');
 }
 
  */
