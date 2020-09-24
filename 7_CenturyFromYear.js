bb20acc5fdd73fc1ba825058acb79340d9156300
Vladimir Kukresh <kreshsoftware@gmail.com>
2020-09-24 23:33:58 +0300

    fix: correct calculation century

--- 7_CenturyFromYear.js
+++ 7_CenturyFromYear.js
@@ -5,14 +5,20 @@ function century(inYear) {
   const newYear = String(inYear);
 
   const year = newYear.split('').join('');
+  const len = year.length;
 
-  if (`${year[2]}${year[3]}` === String('00')) {
-    century = Number(inYear) / 100;
+  if (`${year[len - 2]}${year[len - 1]}` === String('00')) {
+    century = Math.round(Number(inYear) / 100);
+  } else if (year.length < 3) {
+    century = 1;
+  } else if (year.length > 4) {
+    century = Math.ceil(Number(inYear) / 100);
   } else {
-    centuryEnd = `1${Number(year[1]) + 1}00`;
+    centuryEnd = `${year[0]}${Number(year[1]) + 1}00`;
 
-    century = centuryEnd / 100;
+    century = Math.round(centuryEnd / 100);
   }
 
   return century;
 }
+
