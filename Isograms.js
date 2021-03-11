8d988528f2c94b65a58c33220a5d0bfbe3d970d5
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-03-11 23:17:34 +0300

    feat: made function isIsogram

--- Isograms.js
+++ Isograms.js
@@ -1,3 +1,13 @@
-function isIsogram(str){
-    //...
+/* eslint-disable no-plusplus */
+function isIsogram(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str.toLowerCase().indexOf(str[i]) !== str.toLowerCase().lastIndexOf(str[i])) {
+      return false; // repeats
+    }
   }
+  return true;
+}
+/**
+ * use .indexOf() и .lastIndexOf() , чтобы определить, повторяется ли индекс. Это означает, что если первое вхождение 
+ * символа является также и последним вхождением, то вы знаете, что оно не повторяется. Если это не так,то оно повторяется.
+ */
