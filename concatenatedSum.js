46d2aa7e33d9a2774ab35f2287ad89994dcd6f64
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-03-16 00:04:37 +0300

    init: new task

---
+++ concatenatedSum.js
@@ -0,0 +1,6 @@
+/*
+function :-checkConcatenatedSum
+inputs:- integers representing original number and number of times each digit should be repeated
+output:- true of concatenated sum gives original number else false
+
+*/
