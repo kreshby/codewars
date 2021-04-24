3b13bf78dfa427c921b69f45383990555563959b
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-04-24 21:37:42 +0300

    feat: make function which slice middle character

--- get_the_middle_character.js
+++ get_the_middle_character.js
@@ -1,4 +1,12 @@
-function getMiddle(s)
-{
-  
+/* eslint-disable no-unused-expressions */
+function getMiddle(s) {
+  let result;
+
+  const strLength = s.length;
+
+  strLength % 2 === 0
+    ? (result = s.slice(strLength / 2 - 1, strLength / 2 + 1))
+    : (result = s.slice((strLength - 1) / 2, (strLength - 1) / 2 + 1));
+
+  return result;
 }
