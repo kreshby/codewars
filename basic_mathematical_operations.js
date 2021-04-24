104b00976d029e6ad6afefe4a6a79b5e2cfe27ba
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-04-24 15:33:34 +0300

    init: init new task

---
+++ basic_mathematical_operations.js
@@ -0,0 +1,8 @@
+/**
+ *The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+  */
+
+function basicOp(operation, value1, value2) {
+  // Code
+}
