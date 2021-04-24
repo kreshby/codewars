20cf0a23f5a829331b41181c2911af26b0e173b9
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-04-24 18:02:06 +0300

    feat:make function

--- printing _array_comma.js
+++ printing _array_comma.js
@@ -4,6 +4,6 @@
 Output: String with comma delimited elements of the array in th same order.
  */
 
-function printArray(array){
-    //show me the code!
-  }
+function printArray(array) {
+  return array.join(',');
+}
