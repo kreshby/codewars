347bc6dc582dae50449b0bcac9ec5baa60bb4088
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-04-25 13:21:12 +0300

    init: new task

---
+++ countWords.js
@@ -0,0 +1,16 @@
+/**
+ * Can you realize a function that returns word count from a given string?
+
+You have to ensure that spaces in string is a whitespace for real.
+What kind of tests we got for your code:
+
+Function have to count words, but not spaces, so be sure that it does right.
+Empty string has no words.
+String with spaces around should be trimmed.
+Non-whitespace (ex. breakspace, unicode chars) should be assumed as delimiter
+Be sure that words with chars like -, ', ` are counted right.
+ */
+
+function countWords(str) {
+  // ...
+}
