1ab136d44b9611db557d672a9cdb20a0b2b21769
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-05-23 00:07:23 +0300

    feat: make VALIDATION sheme

--- prefill_an_array.js
+++ prefill_an_array.js
@@ -14,5 +14,30 @@ where you replace n for the actual value passed to the function.
  */
 
 function prefill(n, v) {
+  if (Number.isInteger(n)) {
+    console.log('True. checking n');
 
+    if (typeof v !== 'undefined' || v !== 0) {
+      console.log('check v');
+    } else {
+      if (typeof v === 'undefined') {
+        console.log('undefined');
+      }
+      if (v === 0) {
+        console.log('v=0');
+      }
+    }
+  } else {
+    console.log('typeError');
+  }
 }
+
+// prefill(3,1);
+
+// prefill(2,"abc");
+
+// !prefill("1", 1); does not work
+
+// prefill(3, prefill(2,'2d'));
+
+// prefill("xyz", 1)
